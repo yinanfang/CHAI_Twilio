ab3907ef7c18a099ea3b09693e705a51759cd442
yinanfang <yinan_fang@hotmail.com>
2015-03-14 18:05:33 -0400

    Set up each page

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,15 @@
 // executes when HTML-Document is loaded and DOM is ready
 $(document).ready(function () {
 
+  // $('body').addClass('loaded');
+
   // Loading screen
   setTimeout(function(){
     $('body').addClass('loaded');
     // $("paper-button[raised]").css({
     //   "background": "green"
     // });
-  }, 3000);
+  }, 2000);
 
 
 
