88109822e60ef1044773948331074032f0b5a605
yinanfang <yinan_fang@hotmail.com>
2015-03-15 00:19:27 -0400

    Can't us ng-model for input. Change to FlatUI

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,8 @@
 // executes when HTML-Document is loaded and DOM is ready
 $(document).ready(function () {
 
+  var URLSend = "./twilio_send.php";
+
   $('body').addClass('loaded');
 
   // Loading screen
@@ -12,7 +14,31 @@ $(document).ready(function () {
   // }, 2000);
 
 
+  // control.html
+  angular.module("controlApp", [])
+    .controller("controller", function($scope, $http) {
+      $scope.inputData = {};
+      $scope.inputData.doClick = function(item, event) {
+        var responsePromise = $http.get("/angularjs-examples/json-test-data.jsp");
+        responsePromise.success(function(data, status, headers, config) {
+          $scope.myData.fromServer = data.title;
+        });
+        responsePromise.error(function(data, status, headers, config) {
+          alert("AJAX failed! " + $scope.numTo);
+        });
 
+        $http.post(URLSend, {msg:'hello word!'}).
+          success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+          }).
+          error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
+
+      };
+    } );
 });
   
 
