2ed1a862d14b34820443eaa8f763d4d2f02d58db
yinanfang <yinan_fang@hotmail.com>
2015-03-15 16:27:25 -0400

    Can't get NodeJS working on Webfaction. PHP is not getting call back

--- js/javascript.js
+++ js/javascript.js
@@ -13,21 +13,25 @@ $(document).ready(function () {
   //   // });
   // }, 2000);
 
-
+ 
   // control.html
   angular.module("controlApp", [])
     .controller("controller", function($scope, $http) {
       $scope.inputData = {};
       $scope.inputData.doClick = function(item, event) {
-        var responsePromise = $http.get("/angularjs-examples/json-test-data.jsp");
-        responsePromise.success(function(data, status, headers, config) {
-          $scope.myData.fromServer = data.title;
-        });
-        responsePromise.error(function(data, status, headers, config) {
-          alert("AJAX failed! " + $scope.numTo);
-        });
+        var data = {};
+        // data.clientName = document.querySelector('#organization').selectedItemLabel;
+        // data.numberTo = document.querySelector('#numberTo').value;
+        // data.message = document.querySelector('#message').value;
+        // data.authKey = document.querySelector('#authKey').value;
+        data.clientName = "CARE";
+        data.numberTo = "9194484206";
+        data.message = "Dance in the rain";
+        data.authKey = "z8ZeEtN6Hs";
 
-        $http.post(URLSend, {msg:'hello word!'}).
+        alert(JSON.stringify(data));
+
+        $http.post(URLSend, data).
           success(function(data, status, headers, config) {
             // this callback will be called asynchronously
             // when the response is available
@@ -36,6 +40,9 @@ $(document).ready(function () {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
           });
+
+
+
 
       };
     } );
--- twilio_send.php
+++ twilio_send.php
@@ -1,16 +1,27 @@
 <?php
-$servername = "web404.webfaction.com:3306";
-$username = "yinanfang";
-$password = "123456Chai";
-$dbname = "chai_protect";
 
-try {
-	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	// set the PDO error mode to exception
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "Connected successfully";
-} catch (PDOException $e) {
-	echo "Connection failed: " . $e->getMessage();
+if (isset($_POST)) {
+	// Variables
+	$clientName = $_POST["name"];
+	$numberTo = $_POST["name"];
+	$message = $_POST["message"];
+	$authKey = $_POST["authKey"];
+
+	header("Status: 200 OK");
+
+	$servername = "web404.webfaction.com:3306";
+	$username = "yinanfang";
+	$password = "123456Chai";
+	$dbname = "chai_protect";
+
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected successfully";
+	} catch (PDOException $e) {
+		echo "Connection failed: " . $e->getMessage();
+	}
 }
 
 // Get the PHP helper library from twilio.com/docs/php/install
@@ -19,7 +30,7 @@ require_once __DIR__ . '/twilio-php/Services/Twilio.php'; // Loads the library
 $sid = "AC424a6be19a10b589c1908ae43e48a736";
 $token = "abcf6bd45a21149a9f21825875bf6937";
 $client = new Services_Twilio($sid, $token);
-$numFrom = "+191959001s74";
+$numFrom = "+19195900174";
 $numTo = "+19194484206";
 // $numTo = "+19192654757";
 
@@ -30,6 +41,11 @@ $client->account->messages->create(array(
 	'To' => $numTo,
 	'Body' => "test",
 	'MediaUrl' => "http://upload.wikimedia.org/wikipedia/commons/e/e5/Vienna_Skyline.jpg",
+	'StatusCallback' => "http://chai.yinanfang.webfactional.com/Twilio/twilio_callback.php",
 ));
 
 ?>
+
+<!--
+200 OK
+500 FAIL -->
