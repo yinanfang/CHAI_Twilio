fed7e0950f34cd629a6e22b4f0c8767e4117080c
yinanfang <yinan_fang@hotmail.com>
2015-03-14 22:58:32 -0400

    Polymer working. Before adding FlatUI

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,15 @@
 // executes when HTML-Document is loaded and DOM is ready
 $(document).ready(function () {
 
-  // $('body').addClass('loaded');
+  $('body').addClass('loaded');
 
   // Loading screen
-  setTimeout(function(){
-    $('body').addClass('loaded');
-    // $("paper-button[raised]").css({
-    //   "background": "green"
-    // });
-  }, 2000);
+  // setTimeout(function(){
+  //   $('body').addClass('loaded');
+  //   // $("paper-button[raised]").css({
+  //   //   "background": "green"
+  //   // });
+  // }, 2000);
 
 
 
