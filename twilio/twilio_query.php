51a44024b9110e4dced1379e428bdd357a7296ec
yinanfang <yinan_fang@hotmail.com>
2015-03-29 09:53:51 -0400

    Working copy Mar. 29

--- twilio/twilio_query.php
+++ twilio/twilio_query.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/log/Log.class.php";
 require_once __DIR__ . "/PDO/Db.class.php";
+require_once __DIR__ . '/twilio-php/Services/Twilio.php'; // Loads the library
 
 $settings = parse_ini_file(__DIR__ . "/twilio_settings.ini.php");
 $AUTH_KEY = $settings["AUTH_KEY"];
@@ -12,17 +13,62 @@ if (isset($_POST["AuthKey"]) && strcmp($authKey, $AUTH_KEY) !== 0) {
 	if (isset($_POST["Type"])) {
 		if ($_POST["Type"] == "billing") {
 			$log->write("Received query request for billing");
+			if (isset($_POST["Range"])) {
+				// Querying for something
+				# code...
+			} else {
+				// General query. Send everyting back
+				// Creates the instance
+				$db = new Db();
+				$response = $db->query(
+					"SELECT ClientName, Number, SID, Token FROM Client GROUP BY ClientName"
+				);
+				echo (var_dump($response));
+				echo "start looping \n";
+				for ($i = 0; $i < sizeof($response); $i++) {
+					$sid = $response[$i]["SID"];
+					$token = $response[$i]["Token"];
+					$client = new Services_Twilio($sid, $token);
+					// Loop over the list of records and echo a property for each one
+					foreach ($client->account->usage_records->monthly as $record) {
+						echo $record->price . "; ";
+					}
+					echo "\n";
+					// echo (var_dump($client->account->usage_records));
+
+				}
+
+				// Your Account Sid and Auth Token from twilio.com/user/account
+				// $client = new Services_Twilio($sid, $token);
+				// $numTo = $numberTo;
+				// $numTo = "+19192654757";
+				// $numFrom = "+19195900174";
+
+				// $data = array(
+				// 	'From' => $numFrom,
+				// 	'To' => $numTo,
+				// 	'Body' => $_POST["Body"],
+				// 	'StatusCallback' => "http://chai.yinanfang.webfactional.com/Twilio/twilio/twilio_callback.php",
+				// 	// 'StatusCallback' => "http://protectthem.152.23.4.176.xip.io/twilio_callback.php",
+				// );
+				// // Add mediaUrl if there's one
+				// if (isset($_POST["MediaUrl"])) {
+				// 	$data["MediaUrl"] = $_POST["MediaUrl"];
+				// }
+				// $client->account->messages->create($data);
+
+			}
 
 			// Query for billing
-			$db = new Db();
-			$numFromArray = $db->query(
-				"SELECT Number FROM Client WHERE 1",
-				array(
-					"clientID" => $clientName,
-				));
-			$response["data"] = $numFromArray;
-			header("Status: 200 OK");
-			echo json_encode($response);
+			// $db = new Db();
+			// $numFromArray = $db->query(
+			// 	"SELECT Number FROM Client WHERE 1",
+			// 	array(
+			// 		"clientID" => $clientName,
+			// 	));
+			// $response["data"] = $numFromArray;
+			// header("Status: 200 OK");
+			// echo json_encode($response);
 			return;
 		} else {
 			$logMessage = "Uknown Type";
