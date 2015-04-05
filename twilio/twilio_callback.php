fc1249869ae4bee1ab7fb0a64d0aaa915998e68f
yinanfang <yinan_fang@hotmail.com>
2015-04-05 09:44:56 -0400

    Twilio Request working. Before changing the call back php

--- twilio/twilio_callback.php
+++ twilio/twilio_callback.php
@@ -7,41 +7,46 @@ $log = new Log();
 $log->write("Received Callback in " . basename(__FILE__));
 
 try {
-	$response = array();
-	$request = file_get_contents('php://input');
-	// $log->write("the whole thing: " . $request);
-	$parts = explode("&", $request);
-	foreach ($parts as $part) {
-		$items = explode("=", $part);
-		$response[$items[0]] = $items[1];
-	}
-	// Logging response data
-	// foreach ($response as $key => &$value) {
-	// 	error_log("json: " . $key . " -> " . $value);
-	// }
+  $response = array();
+  $request = file_get_contents('php://input');
+  $rrr = print_r($_REQUEST);
+  $log->write("the whole thing: " . $request . $_REQUEST["From"] . $rrr);
+  foreach ($_REQUEST as $key => $value) {
+    $log->write("request: " . $key . "; " . $value);
+  }
 
-	// Get variables
-	$MessageSid = $response["MessageSid"];
-	$ErrorCode = $response["ErrorCode"];
-	$MessageStatus = $response["MessageStatus"];
-	$log->write("MessageSid: " . $response["MessageSid"] . " MessageStatus: " . $MessageStatus);
+  $parts = explode("&", $request);
+  foreach ($parts as $part) {
+    $items = explode("=", $part);
+    $response[$items[0]] = $items[1];
+  }
+  // Logging response data
+  // foreach ($response as $key => &$value) {
+  //  error_log("json: " . $key . " -> " . $value);
+  // }
 
-	// Update status
-	$db = new Db();
-	$update = $db->query(
-		"	UPDATE Message
-			SET MessageStatus = :MessageStatus, ErrorCode = :ErrorCode
-			WHERE MessageSid = :MessageSid",
-		array(
-			"MessageStatus" => $MessageStatus,
-			"ErrorCode" => $ErrorCode,
-			"MessageSid" => $MessageSid,
-		)
-	);
+  // Get variables
+  $MessageSid = $response["MessageSid"];
+  $ErrorCode = $response["ErrorCode"];
+  $MessageStatus = $response["MessageStatus"];
+  $log->write("MessageSid: " . $response["MessageSid"] . " MessageStatus: " . $MessageStatus);
+
+  // Update status
+  $db = new Db();
+  $update = $db->query(
+    " UPDATE Messages
+      SET MessageStatus = :MessageStatus, ErrorCode = :ErrorCode
+      WHERE MessageSid = :MessageSid",
+    array(
+      "MessageStatus" => $MessageStatus,
+      "ErrorCode" => $ErrorCode,
+      "MessageSid" => $MessageSid,
+    )
+  );
 
 } catch (Exception $e) {
-	$logMessage = $e->getMessage();
-	$log->write($logMessage);
+  $logMessage = $e->getMessage();
+  $log->write($logMessage . "\r\n");
 }
 
 ?>
